8231027a2ae715399c0a37bcfe00685caa2f2ec1
zhanhunyiran <2860015667@qq.com>
2016-01-09 21:27:20 +0800

    zigzag update

--- leetcode/ZigZag Conversion/test1.c
+++ leetcode/ZigZag Conversion/test1.c
@@ -1,23 +1,15 @@
-public class Solution {
-    public String convert(String s, int nRows) {
-        int len = s.length();
-        if (len == 0 || nRows < 2) return s;
-        
-        String ret = "";
-        int lag = 2*nRows - 2; //循环周期
-        for (int i = 0; i < nRows; i++) {
-            for (int j = i; j < len; j += lag) {
-                ret += s.charAt(j);
-                
-                //非首行和末行时还要加一个
-                if (i > 0 && i < nRows-1) {
-                    int t = j + lag - 2*i;
-                    if (t < len) {
-                        ret += s.charAt(t);
-                    }
-                }
-            }
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows<2 || s.size()<=numRows) return s;
+        string res(s.size(),0);
+        char *p=&res[0];
+        for(int i=0, j,k, step=(numRows-1)*2;i<numRows;++i) {
+            if(i==0 || i==numRows-1) 
+                for(j=i;j<s.size();j+=step) *p++=s[j];
+            else
+                for(j=i, k=i<<1;j<s.size();k=step-k,j+=k) *p++=s[j];
         }
-        return ret;
+        return res;
     }
-}
+};
